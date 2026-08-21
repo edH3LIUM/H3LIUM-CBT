6fd987c2c5bed48c435ba5c67031663782f594dc
edH3LIUM <h3liumop@gmail.com>
2026-08-21 13:18:36 +0530

    Update MainActivity.java

--- app/src/main/java/com/h3lium/cbt/MainActivity.java
+++ app/src/main/java/com/h3lium/cbt/MainActivity.java
@@ -8,26 +8,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private WebView webView;
+    private WebView myWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        webView = findViewById(R.id.webview);
-        WebSettings webSettings = webView.getSettings();
+        myWebView = findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        
+        // Web site features enable
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowContentAccess(true);
 
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://h3liumcbt.netlify.app/");
+        // Internal navigation within the app
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        // Load Netlify Web App
+        myWebView.loadUrl("https://h3liumcbt.netlify.app/"); 
     }
 
+    // Handle back button for in-app navigation
     @Override
     public void onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack();
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
         } else {
             super.onBackPressed();
         }
